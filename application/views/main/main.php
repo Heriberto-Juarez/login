c7825b8a51e11686ea39f2e7f8640a34b3611a57
Herii <heribertojuarezj@gmail.com>
2020-03-31 04:33:21 -0600

    Formulario sin head ni footer en el archivo

--- application/views/main/main.php
+++ application/views/main/main.php
@@ -1,13 +1,33 @@
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport"
-		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Login</title>
-</head>
-<body>
-
-</body>
-</html>
+<div class="container">
+	<div class="row">
+		<main class="col-lg-4 offset-lg-4 mt-5">
+			<form class="card py-3 shadow-lg">
+				<div class="card-title">
+					<h1 class="text-center">Iniciar sesión</h1>
+					<p class="text-muted text-center">Para acceder al sistema debes iniciar sesión</p>
+				</div>
+				<div class="card-body">
+					<div class="input-group mb-3">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-envelope"></i></span>
+						</div>
+						<input type="email" class="form-control" id="email" name="email"
+							   placeholder="Correo electrónico" aria-label="Correo electrónico">
+					</div>
+					<div class="input-group mb-3">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+						<input type="password" class="form-control" id="password" name="password"
+							   placeholder="Contraseña" aria-label="Contraseña">
+					</div>
+					<div class="input-group mb-3">
+						<div class="btn btn-tomato w-100">
+							Iniciar sesión
+						</div>
+					</div>
+				</div>
+			</form>
+		</main>
+	</div>
+</div>
