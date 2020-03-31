e4da8d66cf155749abb90dc687910d81e1cf7a28
Herii <heribertojuarezj@gmail.com>
2020-03-31 04:33:47 -0600

    Seccion basicas del esqueleto html

--- application/views/component/skeleton/footer.php
+++ application/views/component/skeleton/footer.php
@@ -1,1 +1,5 @@
-<?php
+<script src="<?= base_url() ?>node_modules/jquery/dist/jquery.min.js"></script>
+<script src="<?= base_url() ?>node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url() ?>node_modules/animejs/lib/anime.min.js"></script>
+</body>
+</html>
--- application/views/component/skeleton/head.php
+++ application/views/component/skeleton/head.php
@@ -1,1 +1,13 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Login</title>
+	<link rel="stylesheet" href="<?= base_url() ?>node_modules/@fortawesome/fontawesome-free/css/all.min.css">
+	<link rel="stylesheet" href="<?= base_url() ?>node_modules/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?= base_url() ?>css/core.css">
+</head>
+<body>
